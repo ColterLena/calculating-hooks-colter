043f6898088fad304ef4eb9e44061cfe695794ec
Colter Lena <colterlena@verizon.net>
2020-07-01 17:00:10 -0400

    Done with Explorer Mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import HelloWorld from './components/HelloWorld'
+import React, { useState } from 'react'
+import { Calculator } from './components/Calculator'
 
 function App() {
-  return <HelloWorld />
+  return <Calculator />
 }
 
 export default App
